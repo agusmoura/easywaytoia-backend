ede857f236b64568ba2fcd1725f7a0b78e4cce9c
Agustin Mouriño <agus442m@gmail.com>
2025-01-04 15:05:11 -0300

    fix: revert token and checkout URLs in PaymentUala service to production endpoints
    - Changed the token URL in the getToken method back to the production endpoint, ensuring the application interacts with the correct API for live transactions.
    - Updated the checkout URL to the production endpoint, aligning the service with the intended operational environment for payment processing.

--- app/Services/Payments/PaymentUala.php
+++ app/Services/Payments/PaymentUala.php
@@ -197,8 +197,7 @@ class PaymentUala
 
     private static function getToken()
     {
-        // $url= "https://auth.developers.ar.ua.la/v2/api/auth/token";
-        $url= "https://auth.stage.developers.ar.ua.la/v2/api/auth/token";
+        $url= "https://auth.developers.ar.ua.la/v2/api/auth/token";
         $username = config('services.uala.username');
         $clientId = config('services.uala.client_id');
         $clientSecret = config('services.uala.client_secret');
@@ -248,7 +247,7 @@ class PaymentUala
             self::getToken();
         }
 
-        $url = "https://checkout.stage.developers.ar.ua.la/v2/api/checkout";
+        $url = "https://checkout.developers.ar.ua.la/v2/api/checkout";
 
         $response = Http::withoutVerifying()->withHeaders([
             'Authorization' => 'Bearer ' . self::$accessToken,
